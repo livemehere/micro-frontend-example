c9ca6aa95cf51d39c0cde47a2812af4c7d921a72
livemehere <rhdxoals@gmail.com>
2023-08-12 15:49:23 +0900

    tsconfig path wildcard

--- packages/host/src/pages/index.tsx
+++ packages/host/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import dynamic from "next/dynamic";
 
 const NiceButton = dynamic(() => import("ui/NiceButton"),{ ssr: false });
+const NiceInput = dynamic(() => import("ui/NiceInput"),{ ssr: false });
+
 export default function Home() {
   return (
     <>
         <div>HI</div>
         <NiceButton text={'HI'} color={'red'}/>
+        <NiceInput/>
     </>
   )
 }
